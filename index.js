d1065b0508ebcebdc34a317a5e51af2e7f7fd295
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-23 00:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(katzDeliLine, name) {
   var lineLength = katzDeliLine.length + 1;
   return (`Welcome, ${name}. You are number ${lineLength} in line`);
 }
+
+function nowServing(katzDeliLine, name) {
+  katzDeliLine.shift[0];
+}
