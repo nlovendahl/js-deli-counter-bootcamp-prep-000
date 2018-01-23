46d184a21a4cb312f1fcf1743135dfd08f11d0ff
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-23 01:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length <= 0) {
+  while (katzDeliLine.length <= 0) {
     return `There is nobody waiting to be served!`;
   } else {
      return `Currently serving ${katzDeliLine[0]}.`;
