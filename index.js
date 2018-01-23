ff1c96a9debd3891437ce65e94f2c99bd84c7b41
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-23 01:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function nowServing(katzDeliLine, name) {
 
 function currentLine(katzDeliLine) {
   var line = [];
-  if (katzDeliLine.length < 0) {
+  if (katzDeliLine.length <= 0) {
     return `The line is currently empty.`;
   } else {
-    for (var i = 0; i < katzDeliLine.length; i++) {
-  line.push(`${i + 1}. ${katzDeliLine[i]}`);
+    for (var i = 1; i < katzDeliLine.length; i++) {
+  line.push(`${i + 1}. ${katzDeliLine[i]}` + ' ');
   }
   return `The line is currently: ${line}`;
 }
