2af008e901ed12ed553966aedc2a3048b0848ea2
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-23 00:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function takeANumber(katzDeliLine, name) {
-  for (var i = 0; i < katzDeliLine.length; i++) {
-    var a = katzDeliLine[i];
-    var greeting = `Welcome, ${name}. You are number ${a} in line.`;
-  }
-  return greeting;
+  
 }
